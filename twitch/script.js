4a8b8b2872bf6c4c7cb1334420a47763d40c34da
Marc Davenport <rmarcus.davenport@gmail.com>
2018-05-26 19:20:30 -0500

    Cleaned out old code

--- twitch/script.js
+++ twitch/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
     //streamObj holds the stream names, set to defaults
-    var streamObj = ["OverwatchLeague","freecodecamp", "riotgames", "trumpsc", "kittyplaysgames", "devwars", "lirik", "robotcaleb", "sodapoppin", "leveluplive", "streamerhouse", "noobs2ninjas",  "reynad27", "gassymexican"];
-
+    var streamObj = ["OverwatchLeague"/*,"freecodecamp", "riotgames", "trumpsc", "kittyplaysgames", "devwars", "lirik", "robotcaleb", "sodapoppin", "leveluplive", "streamerhouse", "noobs2ninjas",  "reynad27", "gassymexican"*/];
+    var testObj = [];
     //This variable has three possible states, 0, 1, or 2
-    //It is used to show All, Offline Only, or Online Only
+    //It is used to show All, Offline Only, or Online Ondly
     //0 corresponds to All, 1 to Offline, 2 to Online
     var show = 0;
     var wait;
@@ -68,7 +68,6 @@ $(document).ready(function() {
     headers: {'Client-ID': 'x7fhewv9s5ohjlcp7ke8lfeoppsa5d'},
     data: {'login': value},
     success: function(channel){
-      console.log(channel.data[0])
       $('#title' + index).text(channel.data[0].display_name);
       if($('#overlay' + index + ' p').html() == ""){
         channel.data[0].description ? $('#overlay' + index + ' p').html(channel.data[0].description) : $('#overlay' + index + ' p').html('No Description Available');
@@ -87,49 +86,6 @@ $('.stream').css('opacity', '1');
 }, function(){*/
   //console.log('error');
 });
-        /*    $.getJSON('https://api.twitch.tv/helix/users?login=' + value + '?client_id=x7fhewv9s5ohjlcp7ke8lfeoppsa5d&callback=?').then(function(status) {
-                if (status.stream === null) {
-                    //if Offline only, create DOM element
-                    if (show === 1) {
-                        $('#the-end').before("<a id=\'link" + index + "\'- href=# target=\'_blank\'>" +
-                            "<div class=\'stream col-lg-1 col-md-3 col-xs-6\' id=\'stream" + index + "\'>" +
-                            "<img class=\'logo\' id=\'logo" + index + "\'><div class=\'overlay\' id=\'overlay" + index + "'\>" +
-                            "<i class=\'fa fa-close\' id=\'" + index + "\'></i></div></img>" +
-                            "<p class=\'title\' id=\'title" + index + "\'></p></div></a>");
-                    }
-                    $('#stream' + index).css('border-color', '#FF1744');
-
-                } else {
-                    //if Online only, create DOM element
-                    if (show === 2) {
-                        $('#the-end').before("<a id=\'link" + index + "\'- href=# target=\'_blank\'>" +
-                            "<div class=\'stream col-lg-1 col-md-3 col-xs-6\' id=\'stream" + index + "\'>" +
-                            "<img class=\'logo\' id=\'logo" + index + "\'><div class=\'overlay\' id=\'overlay" + index + "'\>" +
-                            "<i class=\'fa fa-close\' id=\'" + index + "\'></i></div></img>" +
-                            "<p class=\'title\' id=\'title" + index + "\'></p></div></a>");
-                    }
-                    $('#stream' + index).css('border-color', '#00E676');
-                }
-
-                //When the status promise is fulfilled,
-                //retrieve the remaining details for visible cards
-
-                return $.getJSON('https://api.twitch.tv/helix/users?login=' + value);
-            }).then(function(channel) {
-                $('#title' + index).text(channel.display_name);
-                $('#overlay' + index + ' p').html(channel.status);
-                if (channel.logo === null) {
-                    //Placeholder image if no logo is set
-                    $('#logo' + index).attr('src', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/231853/TwitchHolder.png');
-                } else {
-                    $('#logo' + index).attr('src', channel.logo);
-                }
-                $('#link' + index).attr('href', channel.url);
-            }).then(function(){
-              $('.stream').css('opacity', '1');
-            }, function(){
-              console.log('error');
-            }); */
         }
     //**********************
     //End primary function *
